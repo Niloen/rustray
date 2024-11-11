2209c202870611e777d33a05af5661853678ab39
Marcus Nilsson <marcus.nilsson@niloen.com>
2024-11-11 15:57:19 +0100

    Output photo time

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::thread;
+use std::time::Instant;
 use async_channel::{Sender};
 use crate::camera::Camera;
 use crate::vector::Vector3;
@@ -39,13 +40,13 @@ fn main() -> glib::ExitCode {
         image_widget.set_halign(Align::Fill);
         image_widget.set_valign(Align::Fill);
         image_widget.set_vexpand(true);
-        
+
         let container = Box::new(Orientation::Vertical, 0);
         container.append(&Label::new(Some("hello world")));
         container.append(&image_widget);
         container.set_hexpand(true);
         container.set_vexpand(true);
-        
+
         window.set_child(Some(&container));
         window.present();
 
@@ -71,15 +72,18 @@ fn generate_image(width: u32, height: u32, tx: Sender<(u32, u32, Rgb<u8>)>) {
     world.add(Sphere::new(Vector3::new(0.0, 0.0, 100.0), 20.0, Rgb([1.0, 0.0, 0.0])));
     for i in 1..=1000 {
         let ifl = i as f64;
-        
+
         world.add(Sphere::new(Vector3::new(20.0 + ifl, 0.5, 200.0 - ifl * 3.0), 50.0, Rgb([0.0, 1.0, ifl / 1000.0])));
     }
 
     let camera_base = Ray::new(Vector3::new(0.0, 0.0, 0.0), Vector3::new(0.0, 0.1, 1.0).normalize());
     let camera = Camera::new(camera_base, width, height, 50.0);
 
+    let photo_start_time = Instant::now();
     let image = camera.take_photo(&world, tx);
-
+    let photo_duration = photo_start_time.elapsed();
+    println!("Photo generation completed in: {:?}", photo_duration);
+    
     image.save("output.png").unwrap();
     println!("Generated image");
 }
