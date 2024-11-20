b8612fa2ddf24e6bb2fd34b240cc09c4392b06b2
Marcus Nilsson <marcus.nilsson@niloen.com>
2024-11-20 13:22:47 +0100

    Removed send trait for caster

--- src/scene/ray.rs
+++ src/scene/ray.rs
@@ -26,7 +26,7 @@ impl Ray {
     }    
 }
 
-pub trait RayCaster: Send + Sync {
+pub trait RayCaster: Sync {
     fn cast(&self, ray: &Ray, depth: u32) -> Rgb<f64>;
     
     fn direct_lightning(&self, normal_ray: &Ray) -> Rgb<f64>;
