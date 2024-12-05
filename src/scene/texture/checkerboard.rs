5a1e52f365cb03adf30c7e2009c61c97e7b9a8b2
Marcus Nilsson <marcus.nilsson@niloen.com>
2024-12-05 11:25:24 +0100

    Optimized surface_at

--- src/scene/texture/checkerboard.rs
+++ src/scene/texture/checkerboard.rs
@@ -19,15 +19,41 @@ impl CheckerboardTexture {
 impl Texture for CheckerboardTexture {
     fn surface_at(&self, coords: TextureCoords) -> Surface {
         let (u, v) = coords;
-        let checker = ((u * self.scale).floor() + (v * self.scale).floor()) as i32;
-        if checker % 2 == 0 {
+
+        // Combine calculations for the checker index
+        let checker = ((u * self.scale).floor() as i32 + (v * self.scale).floor() as i32) & 1;
+
+        // Use the result of the bitwise AND to select the surface
+        if checker == 0 {
             self.surface1
         } else {
             self.surface2
         }
     }
 
+
     fn clone_box(&self) -> Box<dyn Texture> {
         return Box::new(self.clone());
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::time::Instant;
+    use crate::scene::Color;
+    use crate::scene::material::Material;
+    use super::*;
+    
+    #[test]
+    fn bench() {
+        let s1 = Surface::new(Color::from([1.0, 0.0, 0.0]), Material::DEFAULT);
+        let s2 = Surface::new(Color::from([0.0, 1.0, 0.0]), Material::DEFAULT);
+        let t = CheckerboardTexture::new(s1, s2, 1.0);
+        
+        let time = Instant::now();
+        for _ in 0..100000000 {
+            t.surface_at((0.5, 0.5));
+        }
+        println!("{:?}", time.elapsed());
     }
 }
